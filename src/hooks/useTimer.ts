bd8f98ab494b441d5dc8dbc18b5cd737b3ffa21c
Mahkambek Satimov <mahkambeksatimov@gmail.com>
2025-07-16 00:21:19 +0500

    fix: adjust timer condition to handle all cases under 2 minutes

--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -54,7 +54,7 @@ export function useTimer({
     const hours = Math.floor(timeLeft / 3600);
     const minutes = Math.ceil((timeLeft % 3600) / 60);
     const seconds = timeLeft % 60;
-    if (hours === 0 && minutes === 2) {
+    if (hours === 0 && minutes <= 2) {
       const minutes = Math.floor((timeLeft % 3600) / 60);
       return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
     }
