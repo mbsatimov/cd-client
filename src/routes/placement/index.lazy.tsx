171b5d801028b687670114c20422e1874cbbc651
Mahkambek Satimov <mahkambeksatimov@gmail.com>
2025-09-10 16:24:02 +0500

    refactor: add logo for placement form

--- src/routes/placement/index.lazy.tsx
+++ src/routes/placement/index.lazy.tsx
@@ -1,34 +1,30 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { createLazyFileRoute } from '@tanstack/react-router';
 
-import { BaseLayout } from '@/components/layout'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui'
+import { BaseLayout } from '@/components/layout';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui';
 
-import { PlacementForm } from './-components'
+import { PlacementForm } from './-components';
 
 const RouteComponent = () => {
   return (
-    <BaseLayout className="grid h-screen place-items-center">
-      <Card className="w-full max-w-sm">
-        <CardHeader className="flex flex-col space-y-2 text-left">
+    <BaseLayout className='flex h-screen flex-col items-center justify-center gap-4'>
+      <div className='mb-4 flex items-center justify-center'>
+        <img alt='IELTS ZONE' className='h-16 dark:hidden' src='/logo.png' />
+        <img alt='IELTS ZONE' className='hidden h-16 dark:inline' src='/logo-dark.png' />
+      </div>
+      <Card className='w-full max-w-sm'>
+        <CardHeader className='flex flex-col space-y-2 text-left'>
           <CardTitle>Start Placement</CardTitle>
-          <CardDescription>
-            Enter your details to participate in the test
-          </CardDescription>
+          <CardDescription>Enter your details to participate in the test</CardDescription>
         </CardHeader>
         <CardContent>
           <PlacementForm />
         </CardContent>
       </Card>
     </BaseLayout>
-  )
-}
+  );
+};
 
 export const Route = createLazyFileRoute('/placement/')({
-  component: RouteComponent,
-})
+  component: RouteComponent
+});
